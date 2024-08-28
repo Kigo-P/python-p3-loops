1936ab1f4efeea2c8a886f3b1fb77bb1421e1b8d
Kigo-P <peterkigo33@gmail.com>
2024-08-28 17:10:49 +0300

    Uses loops to solve challenges

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,35 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    numbers = 10
+    while numbers >=0:
+        if numbers > 0:
+            print(numbers)
+        else:
+            print("Happy New Year!")
+        numbers -=1
 
+happy_new_year()
 def square_integers(int_list):
     # code goes here!
-    pass
+    integer_list = [integers**2 for integers in int_list ]
+    return integer_list
+    # for integers in int_list:
+    #     print(integers**2)
+
+square_integers([1,2,3,4,5])
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for numbers in range(1, 101, 1):
+        #print(numbers)
+        if numbers%3 == 0 and numbers%5 == 0:
+            print("FizzBuzz")
+        elif numbers%3 == 0:
+            print("Fizz")
+        elif numbers%5 == 0:
+            print("Buzz")
+        else:
+            print(numbers)
+
+fizzbuzz()
